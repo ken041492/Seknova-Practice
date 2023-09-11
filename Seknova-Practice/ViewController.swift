e51cabaaf2086a05787ae1131c0de7327d1e252c
ken041492 <andy041492@gmail.com>
2023-09-11 11:08:46 +0800

    Initial Commit

---
+++ Seknova-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seknova-Practice
+//
+//  Created by imac-1682 on 2023/9/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
